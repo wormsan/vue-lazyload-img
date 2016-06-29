60e9aee73481fe4e4513b43c1266a97f049d6f1e
Serge <374928031@qq.com>
2016-06-29 18:27:51 +0800

    Update vue.lazyimg.js
       还是this.el 为null的问题
    
       在动态组件快速频繁切换的时候，偶尔出现this.el null的情况

--- src/vue.lazyimg.js
+++ src/vue.lazyimg.js
@@ -7,25 +7,26 @@
  * @email: 116682877@qq.com
  */
 var Vue = require('vue')
-Vue.lazyimg ={
-    install: function(Vue,options){
+Vue.lazyimg = {
+    install: function(Vue, options) {
         options = options || {
-            fadein: false,
-            speed: 20,
-            nohori: false
-        }
-        //custom scrollEnd event
-        if(options.speed){
+                fadein: false,
+                speed: 20,
+                nohori: false
+            }
+            //custom scrollEnd event
+        if (options.speed) {
             var cntr = 0
             var lastCntr = 0
             var diff = 0
             var scrollEnd = document.createEvent('HTMLEvents');
-            scrollEnd.initEvent('scrollEnd',true,false)
+            scrollEnd.initEvent('scrollEnd', true, false)
             scrollEnd.eventType = 'message'
-            function enterFrame(){
-                if(cntr != lastCntr){
+
+            function enterFrame() {
+                if (cntr != lastCntr) {
                     diff++
-                    if(diff == 5){
+                    if (diff == 5) {
                         window.dispatchEvent(scrollEnd)
                         cntr = lastCntr
                     }
@@ -33,100 +34,103 @@ Vue.lazyimg ={
                 requestAnimationFrame(enterFrame);
             }
             window.requestAnimationFrame(enterFrame)
-            document.addEventListener('scroll',function(){
+            document.addEventListener('scroll', function() {
                 lastCntr = cntr
                 diff = 0
                 cntr++
-            },true)
+            }, true)
         }
         //compute scroll speed
         var lastPosY = document.body ? document.body.getBoundingClientRect().top : document.head.parentNode.getBoundingClientRect().top
         var lastPosX = document.body ? document.body.getBoundingClientRect().left : document.head.parentNode.getBoundingClientRect().left
         var lastSpeeds = []
         var aveSpeed = 0
-        function getSpeed(el){
+
+        function getSpeed(el) {
             var curPosY = el ? el.getBoundingClientRect().top : 0
-            var curPosX = el ? el.getBoundingClientRect().left: 0
+            var curPosX = el ? el.getBoundingClientRect().left : 0
             var speedY = lastPosY - curPosY
             var speedX = lastPosX - curPosX
-            if(lastSpeeds.length<10){
-                lastSpeeds.push((speedY+speedX)/2)
-            }else{
+            if (lastSpeeds.length < 10) {
+                lastSpeeds.push((speedY + speedX) / 2)
+            } else {
                 lastSpeeds.shift()
-                lastSpeeds.push((speedY+speedX)/2)
+                lastSpeeds.push((speedY + speedX) / 2)
             }
             var sumSpeed = 0
-            lastSpeeds.forEach(function(speed){
+            lastSpeeds.forEach(function(speed) {
                 sumSpeed += speed
             })
-            aveSpeed = Math.abs(sumSpeed/lastSpeeds.length)
+            aveSpeed = Math.abs(sumSpeed / lastSpeeds.length)
             lastPosY = curPosY
             lastPosX = curPosX
         }
-        document.addEventListener('scroll',function(e){
-            if(!options.speed) return
+        document.addEventListener('scroll', function(e) {
+            if (!options.speed) return
             var el = null
-            for(var i=0; i<e.target.childNodes.length; i++){
-                if(e.target.childNodes[i].nodeType == 1){
+            for (var i = 0; i < e.target.childNodes.length; i++) {
+                if (e.target.childNodes[i].nodeType == 1) {
                     el = e.target.childNodes[i]
                     break;
                 }
             }
             getSpeed(el)
-        },true)
+        }, true)
 
         //vue directive update
-        function update(value){
-            if (this.el === null) {
-                return;
-            }
+        function update(value) {
             var isFadeIn = this.modifiers.fadein || options.fadein
             var isNoHori = this.modifiers.nohori || options.nohori
-            if(isFadeIn){
+            if (isFadeIn) {
                 this.el.style.opacity = 0
                 this.el.style.transition = 'opacity .3s'
                 this.el.style.webkitTransition = 'opacity .3s'
             }
-            var compute = function(){
+            var compute = function() {
+                if (this.el === null) {
+                    return;
+                }
                 var rect = this.el.getBoundingClientRect();
                 var vpWidth = document.head.parentNode.clientWidth
                 var vpHeight = document.head.parentNode.clientHeight
-                var loadImg = function(){
+                var loadImg = function() {
                     this.el.src = value
-                    this.el.addEventListener('load',onloadEnd)
-                    window.removeEventListener('scrollEnd',compute,true)
-                    window.removeEventListener('resize',compute,true)
-                    window.removeEventListener('scroll',computeBySpeed,true)
+                    this.el.addEventListener('load', onloadEnd)
+                    window.removeEventListener('scrollEnd', compute, true)
+                    window.removeEventListener('resize', compute, true)
+                    window.removeEventListener('scroll', computeBySpeed, true)
                     lastSpeeds = []
                 }.bind(this)
-                if(this.el.src == value)return
-                if(isNoHori){
-                    if(rect.bottom >=0 && rect.top <= vpHeight){
+                if (this.el.src == value) return
+                if (isNoHori) {
+                    if (rect.bottom >= 0 && rect.top <= vpHeight) {
                         loadImg()
                     }
-                }else if(rect.bottom >=0 && rect.top <= vpHeight
-                        && rect.right >= 0 && rect.left <= vpWidth){
+                } else if (rect.bottom >= 0 && rect.top <= vpHeight && rect.right >= 0 && rect.left <= vpWidth) {
                     loadImg()
                 }
             }.bind(this)
-            var computeBySpeed = function(){
-                if(options.speed && aveSpeed > options.speed)return
+            var computeBySpeed = function() {
+                if (options.speed && aveSpeed > options.speed) return
                 compute()
             }.bind(this)
-            var onload = function(){
+            var onload = function() {
                 compute();
-                this.el.removeEventListener('load',onload)
-                window.addEventListener('scrollEnd',compute,true)
-                window.addEventListener('resize',compute,true)
-                window.addEventListener('scroll',computeBySpeed,true)
+                this.el && this.el.removeEventListener('load', onload)
+                window.addEventListener('scrollEnd', compute, true)
+                window.addEventListener('resize', compute, true)
+                window.addEventListener('scroll', computeBySpeed, true)
             }.bind(this)
-            var onloadEnd = function(){
-                if(isFadeIn)
+            var onloadEnd = function() {
+                if (this.el === null) {
+                    return;
+                }
+                if (isFadeIn)
                     this.el.style.opacity = 1
-                this.el.removeEventListener('load',onloadEnd)
+                this.el.removeEventListener('load', onloadEnd)
             }.bind(this)
-            this.el.addEventListener('load',onload)
+            this.el.addEventListener('load', onload)
         }
-        Vue.directive('lazyload',update)
+        Vue.directive('lazyload', update)
     }
 };
